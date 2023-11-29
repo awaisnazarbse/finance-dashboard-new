78bdf854733746befdb681fad5cf4d072569a5a4
awaisnazarbse <75782401+awaisnazarbse@users.noreply.github.com>
2023-11-29 16:56:30 +0500

    new

--- src/utils/getAllTransactions.js
+++ src/utils/getAllTransactions.js
@@ -1,3 +1,92 @@
+// import axios from "axios";
+
+// export default async function getAllTransactions(
+//   startDate,
+//   endDate,
+//   API_TOKEN,
+//   range = true
+// ) {
+//   // console.log("tran data",{startDate, endDate,API_TOKEN})
+//   const maxRetries = 5;
+//   const retryDelay = 1000;
+//   const pageSize = 100;
+//   let allSales = [];
+//   let pageNumber = 1;
+
+//   const totalPages = await getTotalPages(startDate, endDate, API_TOKEN, range);
+//   console.log("totalPages",totalPages)
+//   if (totalPages === 0) {
+//     return allSales;
+//   }
+
+//   const fetchPage = async (pageNumber) => {
+//     try {
+//       const response = await fetchSalesData(
+//         pageNumber,
+//         startDate,
+//         endDate,
+//         API_TOKEN,
+//         range
+//       );
+//       return response?.transactions || [];
+//     } catch (error) {
+//       if (maxRetries > 0 && error.response && error.response.status === 429) {
+//         console.log(
+//           `Rate limited. Retrying in ${retryDelay / 1000} seconds...`
+//         );
+//         await new Promise((resolve) => setTimeout(resolve, retryDelay));
+//         return fetchPage(pageNumber);
+//       } else {
+//         throw error;
+//       }
+//     }
+//   };
+
+//   const promises = Array.from({ length: totalPages }, (_, i) =>
+//     fetchPage(pageNumber + i)
+//   );
+//   const pageResults = await Promise.all(promises);
+
+//   return pageResults.flat();
+// }
+
+// async function getTotalPages(startDate, endDate, API_TOKEN, range) {
+//   // console.log("pages res", {startDate, endDate, API_TOKEN, range})
+//   const response = await fetchSalesData(
+//     1,
+//     startDate,
+//     endDate,
+//     API_TOKEN,
+//     range
+//   );
+//   return Math.ceil(response?.page_summary?.total / 100) || 0;
+// }
+
+// async function fetchSalesData(
+//   pageNumber,
+//   startDate,
+//   endDate,
+//   API_TOKEN,
+//   range
+// ) {
+//   let url = `https://seller-api.takealot.com/v1/seller/transactions?page_number=${pageNumber}&page_size=100`;
+//   if (range) {
+//     url += `&date_from=${startDate}&date_to=${endDate}`;
+//   } 
+//   console.log("url", url)
+  
+//   const response = await axios.get(url, {
+//     headers: {
+//       Accept: "application/json",
+//       Authorization: `Key ${API_TOKEN}`,
+//     },
+//   });
+
+//     // console.log("response.data",response.data)
+//   return response.data;
+// }
+
+
 import axios from "axios";
 
 export default async function getAllTransactions(
@@ -6,20 +95,19 @@ export default async function getAllTransactions(
   API_TOKEN,
   range = true
 ) {
-  // console.log("tran data",{startDate, endDate,API_TOKEN})
   const maxRetries = 5;
-  const retryDelay = 1000;
+  const initialRetryDelay = 1000; // 1 second
+  const maxRetryDelay = 16000; // 16 seconds
   const pageSize = 100;
   let allSales = [];
-  let pageNumber = 1;
 
   const totalPages = await getTotalPages(startDate, endDate, API_TOKEN, range);
-  console.log("totalPages",totalPages)
+
   if (totalPages === 0) {
     return allSales;
   }
 
-  const fetchPage = async (pageNumber) => {
+  const fetchPage = async (pageNumber, retries = 0) => {
     try {
       const response = await fetchSalesData(
         pageNumber,
@@ -30,12 +118,11 @@ export default async function getAllTransactions(
       );
       return response?.transactions || [];
     } catch (error) {
-      if (maxRetries > 0 && error.response && error.response.status === 429) {
-        console.log(
-          `Rate limited. Retrying in ${retryDelay / 1000} seconds...`
-        );
+      if (retries < maxRetries && error.response && error.response.status === 429) {
+        const retryDelay = Math.min(initialRetryDelay * 2 ** retries, maxRetryDelay);
+        console.log(`Rate limited. Retrying in ${retryDelay / 1000} seconds...`);
         await new Promise((resolve) => setTimeout(resolve, retryDelay));
-        return fetchPage(pageNumber);
+        return fetchPage(pageNumber, retries + 1);
       } else {
         throw error;
       }
@@ -43,7 +130,7 @@ export default async function getAllTransactions(
   };
 
   const promises = Array.from({ length: totalPages }, (_, i) =>
-    fetchPage(pageNumber + i)
+    fetchPage(i + 1)
   );
   const pageResults = await Promise.all(promises);
 
@@ -51,30 +138,16 @@ export default async function getAllTransactions(
 }
 
 async function getTotalPages(startDate, endDate, API_TOKEN, range) {
-  // console.log("pages res", {startDate, endDate, API_TOKEN, range})
-  const response = await fetchSalesData(
-    1,
-    startDate,
-    endDate,
-    API_TOKEN,
-    range
-  );
+  const response = await fetchSalesData(1, startDate, endDate, API_TOKEN, range);
   return Math.ceil(response?.page_summary?.total / 100) || 0;
 }
 
-async function fetchSalesData(
-  pageNumber,
-  startDate,
-  endDate,
-  API_TOKEN,
-  range
-) {
+async function fetchSalesData(pageNumber, startDate, endDate, API_TOKEN, range) {
   let url = `https://seller-api.takealot.com/v1/seller/transactions?page_number=${pageNumber}&page_size=100`;
   if (range) {
     url += `&date_from=${startDate}&date_to=${endDate}`;
-  } 
-  console.log("url", url)
-  
+  }
+
   const response = await axios.get(url, {
     headers: {
       Accept: "application/json",
@@ -82,6 +155,5 @@ async function fetchSalesData(
     },
   });
 
-    // console.log("response.data",response.data)
   return response.data;
 }
